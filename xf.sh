d8f625d861ae467401c818225b2d7c81e5b32aaa
ouyang <67744640+ldj-network@users.noreply.github.com>
2022-04-02 11:22:13 +0800

    Update xf.sh
    add the uuid

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "b7baea05-8b74-45f9-a78b-9e56b26a10e2",
                         "alterId": 0
                     }
                 ],
